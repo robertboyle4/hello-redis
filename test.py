e8f6a3ee3c493db779c57a4d1da6b645c1616820
Robert Boyle <112638672+robertboyle4@users.noreply.github.com>
2023-08-29 17:49:37 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+
+import re
+import sys
+import json
+import os
+
+def test(test1):
+  test2 = os.environ.get("VAR_TEST", "Not Found")
+  print "TEST1: $test1"
+  print "TEST2: $test2"
+
+def initialize_command_line_args(
+    args: Optional[Sequence[str]] = None) -> Optional[argparse.Namespace]:
+  """Initializes and checks all the command-line arguments."""
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "-v", "--var_test", type=str, help="Var test)")
+    
+  parser.set_defaults(make_changes=False)
+  return parser.parse_args(args)
+
+cli = initialize_command_line_args()
+test(cli.var_test)
